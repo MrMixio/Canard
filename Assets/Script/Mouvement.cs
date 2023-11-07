52829a9da4382c5117bf4967517bd821a70925a6
MrMixio <rharlegand@gmail.com>
2023-11-07 16:27:34 +0100

    Rb and asset add

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-    public float maxSpeed = 5.0f;
+    public float maxSpeed = 20f;
     public float rotationSpeed = 2.0f;
-    public float acceleration = 1.0f;
-    public float steeringFactor = 0.2f;
-    public float deceleration = 1.0f;
+    public float acceleration = 10.0f;
+    public float steeringFactor = 1f;
+    public float deceleration = 3f;
 
+    private Rigidbody rb;
     private float currentSpeed = 0.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -29,16 +35,15 @@ public class Mouvement : MonoBehaviour
         if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.E))
         {
             inputRotation = 0.0f;
-
         }
 
         if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.E))
         {
-            currentSpeed = Mathf.MoveTowards(currentSpeed, 0.0f, deceleration * Time.deltaTime);
+            currentSpeed = Mathf.Lerp(currentSpeed, 0.0f, deceleration * Time.deltaTime);
         }
 
         currentSpeed = Mathf.Clamp(currentSpeed + inputSpeed * acceleration * Time.deltaTime, -maxSpeed, maxSpeed);
-        transform.Rotate(Vector3.up, inputRotation * rotationSpeed * Time.deltaTime);
-        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, inputRotation * rotationSpeed * Time.deltaTime, 0));
+        rb.velocity = transform.forward * currentSpeed;
     }
 }
