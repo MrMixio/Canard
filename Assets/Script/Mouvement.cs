9b8951ab119eaea6bf003a32d5cab740fabc38b3
MrMixio <rharlegand@gmail.com>
2023-11-07 16:30:48 +0100

    Add Header

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-    public float maxSpeed = 20f;
-    public float rotationSpeed = 2.0f;
-    public float acceleration = 10.0f;
-    public float steeringFactor = 1f;
+
+    [Header("Vitesse")]
+    [SerializeField] public float maxSpeed = 20f;
+    [SerializeField] public float acceleration = 10.0f;
+
+    [Header("Rotation")]
+    [SerializeField] public float steeringFactor = 1f;
+    [SerializeField] public float rotationSpeed = 2.0f;
+
     public float deceleration = 3f;
 
     private Rigidbody rb;
@@ -21,6 +26,8 @@ public class Mouvement : MonoBehaviour
         float inputSpeed = 0.0f;
         float inputRotation = 0.0f;
 
+
+
         if (Input.GetKey(KeyCode.Z))
         {
             inputSpeed = 1.0f;
@@ -32,6 +39,8 @@ public class Mouvement : MonoBehaviour
             inputRotation = -steeringFactor;
         }
 
+
+        // ------- Gestion des input same moment -------
         if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.E))
         {
             inputRotation = 0.0f;
@@ -45,5 +54,7 @@ public class Mouvement : MonoBehaviour
         currentSpeed = Mathf.Clamp(currentSpeed + inputSpeed * acceleration * Time.deltaTime, -maxSpeed, maxSpeed);
         rb.MoveRotation(rb.rotation * Quaternion.Euler(0, inputRotation * rotationSpeed * Time.deltaTime, 0));
         rb.velocity = transform.forward * currentSpeed;
+
+        // ------- End -------
     }
 }
